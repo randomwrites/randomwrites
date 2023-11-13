db1e40e568e584b076ae13a5ad6980bd5c211936
randomwrites <150580195+randomwrites@users.noreply.github.com>
2023-11-12 19:02:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-// You can add JavaScript functionality if needed
-// For example, interactive elements, dynamic content loading, etc.
+document.addEventListener('DOMContentLoaded', function () {
+    const opinionForm = document.getElementById('opinion-form');
+
+    opinionForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        const opinionText = document.getElementById('opinion').value;
+        const nameText = document.getElementById('name').value;
+
+        // For simplicity, you can log the opinion to the console.
+        console.log(`Opinion: ${opinionText}\nName: ${nameText}`);
+
+        // You can send this data to the server using AJAX or fetch API.
+        // For this example, we'll just log it to the console.
+    });
+});
