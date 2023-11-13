7ce4bea82fdd96201b8989b9068da0539d5398f5
randomwrites <150580195+randomwrites@users.noreply.github.com>
2023-11-12 21:24:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,13 @@
-body {
-    margin: 0;
-    padding: 0;
-    font-family: 'Arial', sans-serif;
-}
+document.addEventListener('DOMContentLoaded', function () {
+    let layers = document.querySelectorAll('.parallax-layer');
 
-.parallax-container {
-    height: 100vh;
-    overflow-x: hidden;
-    overflow-y: auto;
-    perspective: 1px;
-    perspective-origin: 0% 0%;
-}
+    window.addEventListener('scroll', function () {
+        let scrollPosition = window.scrollY;
 
-.parallax-layer {
-    position: absolute;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-}
-
-.layer1 {
-    background: url('https://opengameart.org/sites/default/files/styles/medium/public/1_37.png') center/cover;
-    transform: translateZ(-1px) scale(2);
-}
-
-.layer2 {
-    background: url('https://opengameart.org/sites/default/files/styles/medium/public/2_21.png') center/cover;
-    transform: translateZ(-2px) scale(3);
-}
-
-.layer3 {
-    background: url('https://opengameart.org/sites/default/files/styles/medium/public/3_16.png') center/cover;
-    transform: translateZ(-3px) scale(4);
-}
-
-.content {
-    position: relative;
-    z-index: 2;
-    padding: 20px;
-    text-align: center;
-    color: #fff;
-}
+        layers.forEach(function (layer, index) {
+            let speed = index + 1;
+            let yPos = -(scrollPosition * speed / 5);
+            layer.style.transform = 'translate3d(0, ' + yPos + 'px, 0)';
+        });
+    });
+});
